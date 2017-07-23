6f1bdf5e2d12090a6c28732a282122c3a3fcf11d
Eric Luong <bassx_159@hotmail.com>
2017-07-23 10:59:54 -0400

    Initialize Abilities
    why u no work

--- src/Card.cc
+++ src/Card.cc
@@ -18,6 +18,11 @@ void Card::use(Board& theBoard, int p, int t) {
     ability->use(theBoard,p,t)
 }
 
+int Card::getCost() { return cost;}
+
+int Card::getAttack() { return -1;}
+int Card::getDefense() { return -1;}
+
 void Card::attack(Unit& target) {}
 
 bool Card::hasAbility() {
@@ -29,7 +34,7 @@ int Card::getAbilityCost() {
     return ability->getCost();
 }
 
-bool Card::isTriggered() {
+int Card::isTriggered() {
     if (hasAbility()) return ability->isTriggered();
     else return false;
 }
@@ -40,20 +45,20 @@ string Card::getDescription() {
 
 
 void Card::addAbility(string name) {
-    ability = listOfAbilities[name];
+    ability = Card::listOfAbilities[name];
 }
 
-static void Card::initializeAbilities() {
+void Card::initializeAbilities() {
 
     // Active Minion Abilities
-    listOfAbilities["NovicePyromancer_Ability"] = make_shared<Ability>(NovicePyromancer_Ability());
-    listOfAbilities["ApprenticeSummoner_Ability"] = make_shared<Ability>(ApprenticeSummoner_Ability());
-    listOfAbilities["MasterSummoner_Ability"] = make_shared<Ability>(MasterSummoner_Ability());
+    Card::listOfAbilities.insert(pair<string,shared_ptr<Ability>>("NovicePyromancer_Ability", make_shared<NovicePyromancer_Ability>());
+    Card::listOfAbilities.insert(pair<string,shared_ptr<Ability>>("ApprenticeSummoner_Ability", make_shared<ApprenticeSummoner_Ability>());
+    Card::listOfAbilities.insert(pair<string,Ability>("MasterSummoner_Ability", make_shared<MasterSummoner_Ability>());
 
     // Triggered Minion Abilities
-    listOfAbilities["FireElemental_Ability"] = make_shared<Ability>(FireElemental_Ability());
-    listOfAbilities["PotionSeller_Ability"] = make_shared<Ability>(PotionSeller_Ability());
-    listofAbilities["Troll_Ability"] = make_shared<Ability>(Troll_Ability());
+    listOfAbilities.insert(pair<string,shared_ptr<Ability>>"FireElemental_Ability", make_shared<FireElemental_Ability>());
+    listOfAbilities.insert(pair<string,shared_ptr<Ability>>("PotionSeller_Ability", make_shared<PotionSeller_Ability>());
+    listOfAbilities.insert(pair<string,shared_ptr<Ability>>("Troll_Ability", make_shared<Troll_Ability>());
 
     // Spell Abilities
 
@@ -65,3 +70,4 @@ static void Card::initializeAbilities() {
 string Card::getEnchantmentDescription() {return "";}
 int Card::getEnchantmentAttack() {return -1;}
 int Card::getEnchantmentDefense() {return -1;};
+string Card::getEnchantmentName() { return "";}
